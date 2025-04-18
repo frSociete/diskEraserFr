dd3739834fdf7f893c0b00f146a50a959e133a58
Bolo101 <iglesias.guillaumedu64@gmail.com>
2025-04-18 13:16:42 +0200

    Added zero filling option

--- code/main.py
+++ code/main.py
@@ -13,6 +13,7 @@ def main():
     parser.add_argument('-f', '--filesystem', choices=['ext4', 'ntfs', 'vfat'], help="Filesystem type to use")
     parser.add_argument('-p', '--passes', type=int, default=5, help="Number of passes for erasure")
     parser.add_argument('--crypto', action='store_true', help="Use cryptographic erasure instead of standard multi-pass method")
+    parser.add_argument('--zero', action='store_true', help="Fill encrypted disk with zero data instead of random data")
     args = parser.parse_args()
     
     # Check for root privileges
